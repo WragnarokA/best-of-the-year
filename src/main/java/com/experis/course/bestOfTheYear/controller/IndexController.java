5e16ae0c1b382b67eeed7ec6371bd62c171ac7df
wilmer <wilmer.arango.a@gmail.com>
2023-11-07 18:23:37 +0100

    Step 2 🎉🎉🎉

--- src/main/java/com/experis/course/bestOfTheYear/controller/IndexController.java
+++ src/main/java/com/experis/course/bestOfTheYear/controller/IndexController.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 @RequestMapping("/")
 public class IndexController {
@@ -22,6 +25,37 @@ public class IndexController {
         String author = "Wilmer";
         model.addAttribute("author", author);
         return "home";
+    }
+
+    @GetMapping("/movies")
+    public String getMovies(Model model) {
+        List<String> movies = getBestMovies();
+        String movieTitles = String.join(", ", movies);
+        model.addAttribute("movieList", movieTitles);
+        return "movies";
+    }
+
+    @GetMapping("/songs")
+    public String getSongs(Model model) {
+        List<String> songs = getBestSongs();
+        String songTitles = String.join(", ", songs);
+        model.addAttribute("songList", songTitles);
+        return "songs";
+    }
+
+
+    private List<String> getBestMovies() {
+        List<String> movies = new ArrayList<>();
+        movies.add("Titolo Film 1");
+        movies.add("Titolo Film 2");
+        return movies;
+    }
+
+    private List<String> getBestSongs() {
+        List<String> songs = new ArrayList<>();
+        songs.add("Titolo Canzone 1");
+        songs.add("Titolo Canzone 2");
+        return songs;
     }
 
 
