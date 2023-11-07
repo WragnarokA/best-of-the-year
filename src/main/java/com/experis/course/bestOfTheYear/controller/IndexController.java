0214fc31933cf852c7ed8fb7a4d5609bccb3de8c
wilmer <wilmer.arango.a@gmail.com>
2023-11-07 16:27:15 +0100

    Step 1

---
+++ src/main/java/com/experis/course/bestOfTheYear/controller/IndexController.java
@@ -0,0 +1,17 @@
+package com.experis.course.bestOfTheYear.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/")
+public class IndexController {
+    @GetMapping
+    @ResponseBody
+    public String index() {
+        return "<html><body><h1>Best of the year by Wilmer<h1><bogy><html>";
+    }
+
+}
